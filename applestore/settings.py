fc7e2cc3bb2015bdd0e2b5953dc5b141989390a8
Saad <muhammadsaadi92.786@gmail.com>
2025-03-11 21:26:46 +0500

    Update Code

--- applestore/settings.py
+++ applestore/settings.py
@@ -133,7 +133,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Payment GateWay Stripe
 
-# Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
